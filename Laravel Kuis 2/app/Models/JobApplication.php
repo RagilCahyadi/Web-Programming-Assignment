82c68c1921bad24e95257e9229b58530c1bce935
Ruziqna Hadikafilardi Muhtarom <ruziqnahm@gmail.com>
2025-07-03 12:06:13 +0700

    First Commit Kuis 2

---
+++ Laravel Kuis 2/app/Models/JobApplication.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use app\Models\JobVacancy;
+
+
+class JobApplication extends Model
+{
+    protected $fillable = [
+        'society_id',
+        'job_vacancy_id',
+        'position_id',
+        'notes',
+        'status' // pending/accepted/rejected
+    ];
+
+    // Relasi ke Society
+    public function society()
+    {
+        return $this->belongsTo(Society::class);
+    }
+
+    // Relasi ke JobVacancy
+    public function jobVacancy()
+    {
+        return $this->belongsTo(JobVacancy::class);
+    }
+
+    // Relasi ke Position (jika ada model terpisah)
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
+}
