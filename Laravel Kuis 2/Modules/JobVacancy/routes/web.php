46232dd2a172e7ea7c3044e461fd0e8660a60b07
Rahmansyah Ragil Cahyadi <ragilccahyadi@gmail.com>
2025-07-05 16:11:11 +0700

    Delete Laravel Kuis 2 directory

--- Laravel Kuis 2/Modules/JobVacancy/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\JobVacancy\Http\Controllers\JobVacancyController;
-
-Route::prefix('job-vacancies')->group(function () {
-    Route::get('/', [JobVacancyController::class, 'index'])->name('job-vacancies.index');
-    Route::get('/{id}', [JobVacancyController::class, 'show'])->name('job-vacancies.show');
-});
